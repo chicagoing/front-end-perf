ec41623c37666e8291cff56c5faf2aea3aa95980
Estella Madison <emadison@blurb.com>
2013-07-05 20:41:34 -0700

    Finished presentation

--- slide_config.js
+++ slide_config.js
@@ -12,12 +12,12 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/egm-tiny-icon.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
     ],
-    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+    theme: ['custom'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
   // Author information
